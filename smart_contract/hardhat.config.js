8a907aa7adeb312d824c5a3c845974f25ed4cb9a
Eliúde <59577079+HelioPC@users.noreply.github.com>
2022-09-20 01:47:35 +0100

    Update API KEY

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,5 +1,3 @@
-// 893e5208aa690ddb4c9d8fb764b8ee146bc3166cd2863f554aad1aa956a1c6f9
-
 require('@nomiclabs/hardhat-waffle')
 
 module.exports = {
@@ -8,7 +6,7 @@ module.exports = {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/AH2V8FlBiIVk5LAgoeKWSauNUsljBdYO',
       accounts: [
-        "893e5208aa690ddb4c9d8fb764b8ee146bc3166cd2863f554aad1aa956a1c6f9"
+        "YOUR_SOLIDITY_ACCOUNT"
       ]
     }
   }
